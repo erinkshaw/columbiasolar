b27c5681c1e0c753f417d57a47138cfa562022b0
Erin Shaw <erin.kimberly.shaw@gmail.com>
2021-03-16 16:16:31 -0400

    more style fixes

---
+++ src/Learn.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Accordion, Container, Header, Segment } from "semantic-ui-react";
+
+import Footer from "./Footer";
+import { FACTS, REASONS } from "./research";
+
+import "./Learn.css";
+
+const factsPanels = FACTS.map((f, i) => ({ ...f, key: i }));
+const reasonPanels = REASONS.map((f, i) => ({ ...f, key: i }));
+
+const Learn = () => (
+  <>
+    <Container></Container>
+    <Container text className="Learn">
+      <Header as="h2">🙋 But wait, won’t all those panels . . .</Header>
+      <Accordion panels={factsPanels} />
+    </Container>
+    <Container text className="Why">
+      <Header as="h2">🤔 But can’t we instead . . .</Header>
+      <Accordion panels={reasonPanels} />
+      <Segment>
+        <p>
+          <b>A note on project impacts</b>: to comply with New York state
+          regulations, Hecate is studying how the solar farm will affect local
+          agriculture, birds, water quality, property values, and dozens of
+          other topics.
+        </p>
+        <p>
+          The studies are ongoing, and expected to be published this summer.
+          We’ll know a lot more about the project’s impact then, and this page
+          will be updated accordingly.
+        </p>
+        <p>
+          Until then, this page is based on academic studies, evidence from
+          other solar projects, and local knowledge about the community.
+        </p>
+      </Segment>
+    </Container>
+    <Footer />
+  </>
+);
+
+export default Learn;
