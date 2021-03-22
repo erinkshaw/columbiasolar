2cb98f2fb2663528d048f4c08e463d7474109142
Erin Shaw <erin.kimberly.shaw@gmail.com>
2021-03-22 12:18:41 -0400

    make mobile responsive

--- src/Nav.js
+++ src/Nav.js
@@ -1,33 +1,56 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { ReactComponent as Logo } from "./logo.svg";
 
 import "./Nav.css";
 
-function Nav({ tab, setTab }) {
-  const TABS = {
-    home: 'Home',
-    learn: 'Learn More',
-    about: 'About Us',
-    participate: 'Get Involved'
+class Nav extends React.Component {
+  constructor() {
+    super();
+
+    this.state = { width: 0 };
+
+    window.addEventListener("resize", this.update);
   };
-  return (
-    <div className="Nav">
-      {Object.keys(TABS).map(currentTab => {
-        const header = TABS[currentTab];
-        return (
-          <Link to={currentTab} className="nav-link" key={currentTab}>
-            <button
-              onClick={() => setTab(currentTab)}
-              className={`${tab === currentTab ? 'disabled' : ''}`}>
-              {header}
-            </button>
-          </Link>
-        );
-      })}
-      <Logo />
-    </div>
-  );
+
+  componentDidMount() {
+    this.update();
+  }
+
+  update = () => {
+    this.setState({ width: window.innerWidth });
+  };
+
+  render() {
+    const { tab, setTab } = this.props;
+    const { width } = this.state;
+    const TABS = {
+      home: 'Home',
+      learn: 'Learn More',
+      about: 'About Us',
+      participate: 'Get Involved',
+    };
+    const TABKEYS = width > 800 ? Object.keys(TABS).reverse() : Object.keys(TABS);
+
+    return (
+      <div className="Nav">
+        <Logo />
+        {TABKEYS.map(currentTab => {
+          const header = TABS[currentTab];
+          return (
+            <Link to={currentTab} className="nav-link" key={currentTab}>
+              <button
+                onClick={() => setTab(currentTab)}
+                className={`${tab === currentTab ? 'disabled' : ''}`}>
+                {header}
+              </button>
+            </Link>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default Nav;
